e584f1a756988b494fa683c99edf28905a2e4c0b
Turf1013 <496528674@qq.com>
2015-01-29 18:45:46 +0800

    Mandrill v1.0 first day

---
+++ Mandrill/format.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+'This file is the format of instructions. Very simple but waste of time.'
+
+class format():
+
+	def __init__(BigEndian = True):
+		
+
+	def 
+
+	def __str__():
+		return 'format of insn'
+
+
+if __name__ == "__main__":
