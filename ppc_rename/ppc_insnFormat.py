d9927de3651034370b5383e125df4649ab2a29cf
Turf1013 <496528674@qq.com>
2015-12-09 00:30:36 +0800

    complete insn format

--- ppc_rename/ppc_insnFormat.py
+++ ppc_rename/ppc_insnFormat.py
@@ -15,9 +15,13 @@ class ppcInsnFormat:
 		PFC.RT		: 	(6, 10),
 		PFC.RD		:	(6, 10),
 		PFC.RS		:	(6, 10),
+		PFC.FRS		:	(6, 10),
+		PFC.FRT		:	(6, 10),
 		PFC.RA		:	(11, 15),
 		PFC.D		:	(16, 31),
 		PFC.SI		: 	(16, 31),
+		PFC.UI		: 	(16, 31),
+		PFC.BF		:	(6, 8),
 	}
 	DForm = insnFormat(name="D", fieldDict=DFormFieldDict)
 	
@@ -35,9 +39,24 @@ class ppcInsnFormat:
 	XFormFieldDict = {
 		PFC.OPCD	: 	(0, 5),
 		PFC.RT		: 	(6, 10),
+		PFC.FRT		: 	(6, 10),
+		PFC.FRS		: 	(6, 10),
+		PFC.BT		: 	(6, 10),
+		PFC.TO		: 	(6, 10),
+		PFC.RS		: 	(6, 10),
 		PFC.RA		:	(11, 15),
+		PFC.FRA		:	(11, 15),
 		PFC.RB		:	(16, 20),
+		PFC.FRB		:	(16, 20),
+		PFC.NB		:	(16, 20),
+		PFC.SH		:	(16, 20),
 		PFC.XO		: 	(21, 30),
+		PFC.SR		: 	(12, 15),
+		PFC.Rc		:	(31, 31),
+		PFC.BF		:	(6, 8),
+		PFC.BFA		:	(11, 13),
+		PFC.TH		:	(7, 10),
+		
 	}
 	XForm = insnFormat(name="X", fieldDict=XFormFieldDict)
 	
@@ -52,12 +71,138 @@ class ppcInsnFormat:
 	}
 	XoForm = insnFormat(name="XO", fieldDict=XoFormFieldDict)
 	
+	IFormFieldDict = {
+		PFC.OPCD	:	(0, 5),
+		PFC.LI		:	(6, 29),
+		PFC.AA		:	(30, 30),
+		PFC.LK		:	(31, 31),
+	}
+	IForm = insnFormat(name="I", fieldDict=IFormFieldDict)
+	
+	BFormFieldDict = {
+		PFC.OPCD	:	(0, 5),
+		PFC.BO		:	(6, 10),
+		PFC.BI		:	(11, 15),
+		PFC.BD		:	(16, 29),
+		PFC.AA		:	(30, 30),
+		PFC.LK		:	(31, 31),
+	}
+	BForm = insnFormat(name="B", fieldDict=BFormFieldDict)
+	
+	ScFormFieldDict = {
+		PFC.OPCD	:	(0, 5),
+		PFC.LEV		:	(20, 26),
+	}
+	ScForm = insnFormat(name="SC", fieldDict=ScFormFieldDict)
+	
+	XlFormFieldDict = {
+		PFC.OPCD	:	(0, 5),
+		PFC.BT		: 	(6, 10),
+		PFC.BA		:	(11, 15),
+		PFC.BB		:	(16, 20),
+		PFC.BO		:	(6, 10),
+		PFC.BF		:	(6, 8),
+		PFC.BI		:	(11, 15),
+		PFC.BFA		:	(11, 13),
+		PFC.XO		:	(21, 30),
+		PFC.LK		: 	(31, 31),
+	}
+	XlForm = insnFormat(name="XL", fieldDict=XlFormFieldDict)
+	
+	XfxFormFieldDict = {
+		PFC.OPCD	:	(0, 5),
+		PFC.RT		: 	(6, 10),
+		PFC.RS		: 	(6, 10),
+		PFC.SPR		:	(11, 20),
+		PFC.TBR		:	(11, 20),
+		PFC.FXM		:	(12, 19),
+		PFC.XO		:	(21, 30),
+	}
+	XfxForm = insnFormat(name="XFX", fieldDict=XfxFormFieldDict)
+	
+	XflFormFieldDict = {
+		PFC.OPCD	:	(0, 5),
+		PFC.FLM		:	(7, 14),
+		PFC.FRB		:	(16, 20),
+		PFC.XO		:	(21, 30),
+		PFC.Rc		: 	(31, 31),
+	}
+	XflForm = insnFormat(name="XFX", fieldDict=XflFormFieldDict)
+	
+	XsFormFieldDict = {
+		PFC.OPCD	:	(0, 5),
+		PFC.RS		: 	(6, 10),
+		PFC.RA		: 	(11, 15),
+		PFC.SHH		:	(16, 20),
+		PFC.XO		:	(21, 29),
+		PFC.SHL		:	(30, 30),
+		PFC.Rc		: 	(31, 31),
+	}
+	XsForm = insnFormat(name="XFX", fieldDict=XsFormFieldDict)
+	
+	AFormFieldDict = {
+		PFC.OPCD	:	(0, 5),
+		PFC.FRT		: 	(6, 10),
+		PFC.FRA		: 	(11, 15),
+		PFC.FRB		:	(16, 20),
+		PFC.FRC		:	(21, 25),
+		PFC.XO		:	(26, 30),
+		PFC.Rc		:	(31, 31),
+	}
+	AForm = insnFormat(name="A", fieldDict=AFormFieldDict)
+	
+	MFormFieldDict = {
+		PFC.OPCD	:	(0, 5),
+		PFC.RS		: 	(6, 10),
+		PFC.RA		: 	(11, 15),
+		PFC.RB		:	(16, 20),
+		PFC.SH		:	(16, 20),
+		PFC.MB		:	(21, 26),
+		PFC.ME		:	(27, 30),
+		PFC.Rc		:	(31, 31),
+	}
+	MForm = insnFormat(name="A", fieldDict=MFormFieldDict)
+	
+	MdFormFieldDict = {
+		PFC.OPCD	:	(0, 5),
+		PFC.RS		: 	(6, 10),
+		PFC.RA		: 	(11, 15),
+		PFC.SHH		:	(16, 20),
+		PFC.MB		:	(21, 25),
+		PFC.ME		:	(21, 25),
+		PFC.XO		:	(26, 29),
+		PFC.SHL		:	(30, 30),
+		PFC.Rc		:	(31, 31),
+	}
+	MdForm = insnFormat(name="A", fieldDict=MdFormFieldDict)
+	
+	MdsFormFieldDict = {
+		PFC.OPCD	:	(0, 5),
+		PFC.RS		: 	(6, 10),
+		PFC.RA		: 	(11, 15),
+		PFC.SH		:	(16, 20),
+		PFC.MB		:	(21, 25),
+		PFC.ME		:	(21, 25),
+		PFC.XO		:	(26, 30),
+		PFC.Rc		:	(31, 31),
+	}
+	MdsForm = insnFormat(name="A", fieldDict=MdsFormFieldDict)
 	
 	fieldDictList = [
+		("I", 	IFormFieldDict),
 		("D",	DFormFieldDict),
 		("DS", 	DsFormFieldDict),
 		("X", 	XFormFieldDict),
 		("XO", 	XoFormFieldDict),
+		("B",	BFormFieldDict),
+		("XL",	XlFormFieldDict),
+		("XFX",	XfxFormFieldDict),
+		("XFL",	XflFormFieldDict),
+		("XS",	XsFormFieldDict),
+		("A",	AFormFieldDict),
+		("M",	MFormFieldDict),
+		("MD",	MdFormFieldDict),
+		("MDS",	MdsFormFieldDict),
 	]
 	
 	@classmethod
